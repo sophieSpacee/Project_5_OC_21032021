4fa0afafb8cb6c18b813636f389220e6316b2074
Sophie Gauthier <sophie.gauthier31@gmail.com>
2021-03-24 16:39:38 +0100

    added color

--- js/panier.js
+++ js/panier.js
@@ -1,72 +1,100 @@
 window.onload = () => {
-    console.log(localStorage);
-let productList = JSON.parse(localStorage.getItem("teddy"));
-console.log(productList);
-console.log(typeof productList);
+  let productList = JSON.parse(localStorage.getItem("teddy"));
+  let teddiesList = [];
+  let showCartItems = () => {
+    for (let item of productList) {
+      let productLine = document.createElement("div");
 
-let firstItem = productList[0];
-console.log(firstItem);
+      let productLineContainer = document.getElementById("products-container");
+      productLineContainer.appendChild(productLine);
+      productLine.classList.add("row", "bg-white", "my-3");
 
+      let productCol1 = document.createElement("div");
+      productCol1.classList.add("col-4");
+      productLine.appendChild(productCol1);
 
+      let productCard = document.createElement("div");
+      productCard.classList.add("card");
+      productCol1.appendChild(productCard);
 
-for(let item of productList){
-    console.log(item)
-    let productLine = document.createElement('div');
+      let teddyImage = document.createElement("img");
+      teddyImage.classList.add("card-image-top");
+      productCard.appendChild(teddyImage);
+      teddyImage.setAttribute("src", item.imageUrl);
 
-    let productLineContainer = document.getElementById('products-container');
-    console.log(productLineContainer)
-    productLineContainer.appendChild(productLine);
-    productLine.classList.add('row', 'bg-white', 'my-3');
+      let productCol2 = document.createElement("div");
+      productCol2.classList.add("col-4", "my-auto");
+      productLine.appendChild(productCol2);
 
-    let productCol1 = document.createElement('div');
-    productCol1.classList.add('col-4');
-    productLine.appendChild(productCol1);
+      let productName = document.createElement("h2");
+      productName.classList.add("card-title");
+      productCol2.appendChild(productName);
+      productName.innerHTML = item.name;
 
-    let productCard = document.createElement('div');
-    productCard.classList.add('card');
-    productCol1.appendChild(productCard);
+      let productColor = document.createElement("p");
+      productColor.classList.add("card-text");
+      productCol2.appendChild(productColor);
+      productColor.innerHTML = item.chosenColor;
 
-    let teddyImage = document.createElement('img');
-    teddyImage.classList.add('card-image-top');
-    productCard.appendChild(teddyImage);
-    teddyImage.setAttribute('src', item.imageUrl)
+      let productCol3 = document.createElement("div");
+      productCol3.classList.add("col-4", "my-auto");
+      productLine.appendChild(productCol3);
 
-    let productCol2 = document.createElement('div');
-    productCol2.classList.add('col-4', 'my-auto');
-    productLine.appendChild(productCol2);
+      let productPrice = document.createElement("p");
+      productPrice.classList.add(
+        "align-middle",
+        "my-auto",
+        "text-right",
+        "mr-2",
+        "price"
+      );
+      productCol3.appendChild(productPrice);
+      productPrice.innerHTML = item.price;
 
-    let productName = document.createElement('h2');
-    productName.classList.add('card-title');
-    productCol2.appendChild(productName);
-    productName.innerHTML = item.name;
+      // Construction de la liste des ID a envoyer au serveur
+      teddiesList.push(item._id);
+    }
+  };
 
-    let productColor = document.createElement('p');
-    productColor.classList.add('card-text');
-    productCol2.appendChild(productColor);
-    productColor.innerHTML = 'couleur'
+  console.log(teddiesList);
 
-    let productCol3 = document.createElement('div')
-    productCol3.classList.add('col-4', 'my-auto');
-    productLine.appendChild(productCol3);
+  showCartItems();
 
-    let productPrice = document.createElement('p');
-    productPrice.classList.add('align-middle', 'my-auto', 'text-right', 'mr-2', 'price');
-    productCol3.appendChild(productPrice);
-    productPrice.innerHTML = item.price;
-
-}
-
-let totalPriceCalculation = (productList) => {
+  let totalPriceCalculation = (productList) => {
     console.log(productList);
     let totalPrice = 0;
-    for (let item of productList){
-        totalPrice = totalPrice + item.price;
-        console.log(item)
+    for (let item of productList) {
+      totalPrice = totalPrice + item.price;
+      console.log(item);
     }
     return totalPrice;
-}
+  };
 
-let totalPriceContainer = document.getElementById('total-price');
-totalPriceContainer.innerHTML = totalPriceCalculation(productList);
+  let totalPriceContainer = document.getElementById("total-price");
+  totalPriceContainer.innerHTML = totalPriceCalculation(productList);
 
-}
+let nom = document.getElementById('lastName').value;
+console.log(nom);
+
+
+    //Construction de l'objet contact a envoyer au serveur 
+    let contact = {
+      firstName: "sophie",
+      lastName: "gauthier",
+      address: "rue du roule",
+      city: "Paris",
+      email: "sophie.lo@gmail.com",
+    };
+  
+    // Construction du jsonBody contenant l'objet contact et la liste des ID commandees. 
+    let jsonBody = {
+        contact,
+        teddiesList
+    }
+    console.log(jsonBody)
+
+  //   //Envoyer les donnees au serveur
+    let request = new XMLHttpRequest();
+    request.open('POST', 'http://localhost:3000/api/teddies');
+    request.send(JSON.stringify(jsonBody));
+};
--- js/product-old.js
+++ js/product-old.js
@@ -4,12 +4,9 @@ window.onload = () => {
   const urlParams = new URLSearchParams(queryString);
   const product_id = urlParams.get("_id");
 
-  console.log(product_id);
-
-  // Promise qui récupère les données API du teddy et lance la fonction showTeddyDetail si tout se passe bien
+  // Promise qui récupère les données API et lance la fonction buildTeddies si tout se passe bien
   let teddy = "";
-  //je ne sais pas a quoi ca sert de declarer teddy
-  let getTeddy = fetch("http://localhost:3000/api/teddies/" + product_id, {
+  let getTeddies = fetch("http://localhost:3000/api/teddies/" + product_id, {
     method: "GET",
   })
     .then((response) => {
@@ -18,18 +15,14 @@ window.onload = () => {
     .then((element) => {
       showTeddyDetail(element);
       teddy = element;
-      console.log(teddy);
-      //je ne sais pas a quoi ca sert de declarer teddy
     })
     .catch((error) => alert("Erreur: " + error));
 
-
-    console.log(teddy);
-
   // Afficher le détail du produit sur la page product.html
+  let teddyColor;
   let showTeddyDetail = (element) => {
-    let teddyName = document.getElementById("teddy-name2");
-    teddyName.innerHTML = element.name;
+    let teddyName2 = document.getElementById("teddy-name2");
+    teddyName2.innerHTML = element.name;
 
     let teddyPicContainer = document.getElementById("teddypic-container");
     teddyPicContainer.setAttribute("src", element.imageUrl);
@@ -41,36 +34,21 @@ window.onload = () => {
     teddyPrice2.innerHTML = element.price;
     teddyPrice2.classList.add("price");
 
-    getColorList(element);
-  };
-
-  // Fonction qui recupere la liste des couleur disponible pour ce teddy et les affiche dans une liste deroulante
-  let getColorList = (element) => {
-    let teddyColor = document.getElementById("teddy-color");
+    teddyColor = document.getElementById("teddy-color");
     let colors = element.colors;
     colors.forEach((color) => {
       let option = document.createElement("option");
       teddyColor.appendChild(option);
       option.innerHTML = color;
     });
-    getChosenColor();
-  };
-
-  // Fonction qui recupere la couleur choisie dans le menu deroulant et qui l'ajoute a l'objet teddy
-  let getChosenColor = (element) => {
-    let teddyColor = document.getElementById("teddy-color");
     teddyColor.addEventListener("change", (event) => {
       let chosenColor = event.target.value;
-      teddy.chosenColor = chosenColor;
-      console.log(chosenColor);
-      console.log( teddy)
-      return chosenColor;
-      
-    });
+      console.log(chosenColor)
+
+   });
+    
   };
 
-console.log(teddy)
-  // Fonction qui ajoute le teddy au panier
   var addTeddyToCart = () => {
     var existing = localStorage.getItem("teddy");
     let teddies;
@@ -83,12 +61,6 @@ console.log(teddy)
     teddies.push(teddy);
     localStorage.setItem("teddy", JSON.stringify(teddies));
     console.log(localStorage.getItem("teddy"));
-
-    showSuccessMessage();
-  };
-
-  // Fonction qui fait apparaitre un message de succes lors de l'ajout au panier
-  let showSuccessMessage = () => {
     let successAlert = document.getElementById("alert-success");
     successAlert.innerHTML =
       "Félicitations ! Vous avez ajouté cet article à votre panier !";
@@ -98,6 +70,11 @@ console.log(teddy)
 
   let addToCartButton = document.getElementById("add-cart");
   addToCartButton.onclick = addTeddyToCart;
+
+  function getColor() {
+    var color = document.getElementById("teddy-color").value;
+    console.log(color);
+  }
 
   // localStorage.clear();
 };
